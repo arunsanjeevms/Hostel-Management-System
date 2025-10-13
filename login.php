a7aa63f2f98ed9b7dd1f997ac1a559318e4c289f
Hema <hemasuthakar1226@gmail.com>
2025-10-13 09:12:44 +0530

    db update

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once 'dbconnect.php';
+require_once 'db.php';
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
